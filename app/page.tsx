f5f7c8e37d71b0c76e11ccfbc5e32efca6ca2861
Kev <kevvgdev@gmail.com>
2025-01-03 19:59:07 +0100

    refactor: turn home page into a server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,12 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-export default function Home() {
-  const [stuff1, setStuff1] = useState<any>([]);
-  useEffect(() => {
-    // declare the data fetching function
-    const fetchData = async () => {
-      const data = await fetch("/api/companies");
-      const data2 = await data.json();
-      console.log(data2);
-      setStuff1(data2);
-    };
-
-    // call the function
-    fetchData()
-      // make sure to catch any error
-      .catch(console.error);
-  }, []);
+export default async function Home() {
+  const res = await fetch(`${process.env.BASE_API_URL}/api/companies`);
+  const companies = await res.json();
 
   return (
     <main>
       <h2>Quartr</h2>
       <p>Trending companies</p>
-      <p>{JSON.stringify(stuff1)}</p>
+      <p>{JSON.stringify(companies)}</p>
     </main>
   );
 }
