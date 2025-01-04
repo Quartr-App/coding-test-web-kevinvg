c9ab6a917e839e65f42261e49c352516540fddfe
Kev <kevvgdev@gmail.com>
2025-01-04 20:27:28 +0100

    feat: use text comp instead of p element

--- ui/organisms/TrendingCompaniesListSection.tsx
+++ ui/organisms/TrendingCompaniesListSection.tsx
@@ -1,11 +1,12 @@
 import { getCompanies } from "@core/api";
+import { Text } from "@ui/atoms";
 import { CompanyListItem } from "@ui/molecules";
 
 export const TrendingCompaniesListSection = async () => {
   const companies = await getCompanies();
 
   if (!companies) {
-    return <p>No companies found...</p>;
+    return <Text>No companies found...</Text>;
   }
 
   return (
