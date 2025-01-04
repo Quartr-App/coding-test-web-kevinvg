6ecc811062002c93777d2fb6f54d66e02c3df7bb
Kev <kevvgdev@gmail.com>
2025-01-04 14:46:50 +0100

    feat: upd env variable + simluate real fetch

--- core/api/companies.ts
+++ core/api/companies.ts
@@ -1,12 +1,14 @@
 import { GetCompaniesResponseType } from "@core/types";
 
 export const getCompanies = async () => {
-  const res = await fetch(`${process.env.BASE_API_URL}/api/companies`);
+  const res = await fetch(`${process.env.BASE_URL}/api/companies`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch companies");
   }
 
   const data = (await res.json()) as GetCompaniesResponseType;
+  // Simulate a real API call
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return data.data;
 };
